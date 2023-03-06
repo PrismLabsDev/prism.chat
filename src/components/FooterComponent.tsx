ef8d47e0fb2a3c98396bdb64d2f7a131713a4f8c
Jack Woodrow <jackwoodrow99@gmail.com>
2023-03-06 13:52:23 -0500

    Chagne footer text

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -2,7 +2,7 @@ export default function FooterComponent() {
 	return (
 		<>
 			<div className="bg-neutral-900 text-white text-center p-5">
-				© 2022 Prism Chat. All rights reserved.
+				© 2022 Prism Chat Labs. All rights reserved.
 			</div>
 		</>
 	);
